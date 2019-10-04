eb00ffa1d8ab49aece0b588b83be2a358fd6d2d9
Alex <work.alex966@gmail.com>
2019-10-04 21:05:13 +0300

    правки + появление\скрытие информации о стоимости услуг

--- main.js
+++ main.js
@@ -23,13 +23,42 @@ document.addEventListener('DOMContentLoaded', () => {
 //     },
 // ]
 // });
+    // console.log( document.querySelector(".show-service-price"));
 
-    $( ".show-service-price" ).click(function(){ 
-      $( "read" ).slideToggle("slow",function()
-      {
-        alert("Slide Toggle Method completed.");
-      });
+    let selector = selector => document.querySelector(selector);
+    let selectorAll = selector => document.querySelectorAll(selector);
+
+    
+    
+    selector(".service-price-list").addEventListener("click",(event)=> {
+        console.log(selector(".service-price-list"));
+        console.log(event.target);
+        if(event.target.classList.contains('show-service-price-active')) {
+            event.target.classList.remove('show-service-price-active')
+            console.log($(event.target).parent().children(".active-service-price-list"));
+            $(event.target).parent().children(".active-service-price-list").slideToggle("slow");
+            // $(event.target).parent()[0].
+            // $( ".show-service-price-active" ).siblings(".active-service-price-list").slideToggle("slow");
+            return;
+        }
+        if(event.target.classList.contains('show-service-price')) {
+                event.target.classList.add('show-service-price-active')
+                // $( ".active-service-price-list" ).siblings().slideToggle("slow");
+                // $( ".show-service-price-active" ).siblings(".active-service-price-list").slideToggle("slow");
+                // return  ;
+                $(event.target).parent().children(".active-service-price-list").slideToggle("slow");
+        }
+        
+    // }
     });
+            // $( ".show-service-price" ).click(function(){ 
+
+            //     $( ".active-service-price-list" ).slideToggle("slow");
+            //   });
+
+
+   
+    
   
 
 });
