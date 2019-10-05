bba9e33015034910065de1e80ac465ea02b371d8
Alex <work.alex966@gmail.com>
2019-10-05 18:18:10 +0300

    до и после

--- main.js
+++ main.js
@@ -1,35 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-// $('.budget-list').slick({
-//     infinite: false,
-//     slidesToShow: 2,
-//     slidesToScroll: 1,
-//     centerMode : true,
-//     initialSlide : 1,
-//     arrows :true,
-//     variableWidth : true,
-//     prevArrow: $('.switch-back'),
-//     nextArrow: $('.switch-forward'),
-//     responsive: [
-//     {
-//         breakpoint: 1921,
-//         settings: 'unslick'
-//     },
-
-//     {
-//         breakpoint: 1201,
-//         settings: 'slick'
-        
-//     },
-// ]
-// });
-    // console.log( document.querySelector(".show-service-price"));
-
     let selector = selector => document.querySelector(selector);
     let selectorAll = selector => document.querySelectorAll(selector);
-
-    
-    
     selector(".service-price-list").addEventListener("click",(event)=> {
         console.log(selector(".service-price-list"));
         console.log(event.target);
@@ -37,28 +9,58 @@ document.addEventListener('DOMContentLoaded', () => {
             event.target.classList.remove('show-service-price-active')
             console.log($(event.target).parent().children(".active-service-price-list"));
             $(event.target).parent().children(".active-service-price-list").slideToggle("slow");
-            // $(event.target).parent()[0].
-            // $( ".show-service-price-active" ).siblings(".active-service-price-list").slideToggle("slow");
             return;
         }
         if(event.target.classList.contains('show-service-price')) {
                 event.target.classList.add('show-service-price-active')
-                // $( ".active-service-price-list" ).siblings().slideToggle("slow");
-                // $( ".show-service-price-active" ).siblings(".active-service-price-list").slideToggle("slow");
-                // return  ;
                 $(event.target).parent().children(".active-service-price-list").slideToggle("slow");
         }
-        
-    // }
     });
-            // $( ".show-service-price" ).click(function(){ 
-
-            //     $( ".active-service-price-list" ).slideToggle("slow");
-            //   });
-
-
-   
     
-  
+
+    // slider
+
+    
+
+$('.reviews-list').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    variableWidth: false,
+    arrows :true,
+    responsive: [
+        {
+            breakpoint: 1101,
+            settings: {
+                variableWidth: false,
+            }
+        },
+    ]
+});
+
+$('.examples-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    swipe:false,
+    arrows :true,
+    responsive: [
+        {
+            breakpoint: 1101,
+            settings: {
+                
+            }
+        },
+    ]
+});
+
+// slider!
+
+$(function(){
+    $(".examples-slider .slick-slide").twentytwenty({
+        no_overlay: true,
+    });
+  });
 
 });
