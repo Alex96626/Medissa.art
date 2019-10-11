cef6f4a7ae9c6df0ba4f7a29e118c170ed724c68
Alex <work.alex966@gmail.com>
2019-10-11 16:18:31 +0300

    фича смены текста доделал

--- main.js
+++ main.js
@@ -21,37 +21,55 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    selector(".service-5-list").addEventListener("click",(event)=>{
-
-        selectorAll(".service-5-item").forEach(function(element,i){
-            element.children[1].classList.remove("service-item-picture-active")
-            if(event.target.classList.contains("service-item-picture")){
-                event.target.classList.add("service-item-picture-active");
-            }
-            if(event.target.parentNode.classList.contains("service-item-picture")){
-                event.target.parentNode.classList.add("service-item-picture-active");
-            }
-            // console.log(selectorAll(".info-service-5"));
+    // selector(".service-5-list").addEventListener("click",(event)=>{
+    //     if(!event.target.closest('.service-item-picture')) return;
             
-            // selectorAll(".info-service-5").classList.remove("info-service-5-active");
-            selectorAll(".info-service-5").forEach(function(elem_text){
-            //     console.log(element.children);
-            //     console.log(elem_text);
-                elem_text.classList.remove("info-service-5-active");
-            //     return elem_text;
-            // // console.log(i);
-            });
-            console.log(event.target);
-            console.log(element);
-            if(event.target === element) {
-                console.log('hello');
-            }
+        selector(".service-5-list").addEventListener("click",(event)=> { 
+            const elem = event.target; 
+            
+            if(!elem.closest('.service-item-picture')) return; 
+            
+            selectorAll(".service-5-item").forEach(function(element) { 
+            element.querySelector('.stage').classList.remove('stage-active'); 
+            element.querySelector('.service-item-picture').classList.remove('service-item-picture-active'); 
+            }); 
+            
+            selectorAll(".info-service-5").forEach(function(element) { 
+            element.classList.remove('info-service-5-active'); 
+            }); 
+            console.log(elem.closest('.service-item-picture').previousElementSibling);
+            
+            elem.closest('.service-item-picture').previousElementSibling.classList.add('stage-active'); 
+            elem.closest('.service-item-picture').classList.add('service-item-picture-active'); 
+            
+            console.log(elem.closest('.service-5-item'));   
+            selector('.wrapper-info-service-5 .info-service-5:nth-child( ' + elem.closest('.service-5-item').id + ')').classList.add('info-service-5-active');
 
         });
 
+        // selectorAll(".service-5-item").forEach(function(element,i){
+        //     element.children[1].classList.remove("service-item-picture-active")
+
+        //     if(event.target.closest("service-item-picture").classList.contains("service-item-picture")){
+        //         event.target.classList.add("service-item-picture-active");
+        //     }
+        //     if(event.target.parentNode.classList.contains("service-item-picture")){
+        //         event.target.parentNode.classList.add("service-item-picture-active");
+        //     }
+        //     selectorAll(".info-service-5").forEach(function(elem_text){
+        //         elem_text.classList.remove("info-service-5-active");
+        //     });
+        //     console.log(event.target);
+        //     console.log(element);
+        //     if(event.target === element) {
+        //         console.log('hello');
+        //     }
+
+        // });
+
         
 
-    });
+    // });
     
 
     // slider
